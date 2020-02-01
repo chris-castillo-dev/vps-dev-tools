eed8852d13b3ea38f5ade814ebc39c2c77278e67
Chris Castillo <46719081+chris-castillo-dev@users.noreply.github.com>
2020-02-01 17:32:50 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "Installing Node"
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+
+echo "Installing Node.js"
+sudo apt-get install nodejs
+
+echo "Installing Gulp and Bower"
+npm install -g gulp bower
